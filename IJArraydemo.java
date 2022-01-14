cc8b4562e56d10522d94766859646e057983e04c
ankitchaudhary1234 <81434922+ankitchaudhary1234@users.noreply.github.com>
2022-01-14 20:32:55 +0530

    Create IJArraydemo.java

---
+++ IJArraydemo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class IJArraydemo {
+  static int arr[]; // static varialbe 
+  public static void main(String[] args) {
+    arr =new int[5];
+    Scanner s = new Scanner(System.in);
+
+    for (int i = 0; i <arr.length ; i++) {
+      arr[i]= s.nextInt();
+    }
+    for (int i = 0; i <arr.length ; i++) {
+      System.out.print(arr[i]);
+    }
+
+  }
+
+}
